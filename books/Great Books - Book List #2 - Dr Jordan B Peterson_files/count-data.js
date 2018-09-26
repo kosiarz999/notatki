a8a000b0c1446a8fc45ef43137be23e843ab1160
kosiarz999 <kos.lukasz.88@gmail.com>
2018-09-26 23:00:07 +0200

    Jordan Peterson books

---
+++ books/Great Books - Book List #2 - Dr Jordan B Peterson_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"0 Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"id":"38374 https:\/\/jordanbpeterson.com\/?p=38374","comments":53},{"id":"37778 https:\/\/jordanbpeterson.com\/?p=37778","comments":79},{"id":"38487 https:\/\/jordanbpeterson.com\/?p=38487","comments":2},{"id":"38224 https:\/\/jordanbpeterson.com\/?p=38224","comments":2},{"id":"38312 https:\/\/jordanbpeterson.com\/?p=38312","comments":107}]});
+}
